c46dda42a24e7faf7b14b43cdb9799c394c979f8
emilienbai <bai.emilien+github@gmail.com>
2021-12-01 18:55:39 +0100

    day 1, part 2

---
+++ day_1/main_part2.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+function main_part2() {
+    const inputFile = fs.readFileSync('input.txt')
+    const fileAsArray = inputFile.toString().split('\n').map(input => Number(input));
+    let increaseCount = 0;
+    for (let i = 3; i < fileAsArray.length; i++) {
+        if (fileAsArray[i] + fileAsArray[i - 1] + fileAsArray[i - 2] > fileAsArray[i - 1] + fileAsArray[i - 2] + fileAsArray[i - 3]) {
+            increaseCount++;
+        }
+    }
+    console.log(increaseCount);
+}
+
+main_part2();
