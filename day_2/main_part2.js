2b8314082da1d41866e027f45114d3e13dfc8f48
emilienbai <bai.emilien+github@gmail.com>
2021-12-02 12:16:32 +0100

    Day 2

---
+++ day_2/main_part2.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+function main_part1() {
+    const commands = fs.readFileSync('input.txt').toString().split('\n').map(line => {
+        const splitted = line.split(' ');
+        return {type: splitted[0], amount: Number(splitted[1])};
+    });
+
+    let coordinates = {x: 0, y: 0, aim: 0};
+    commands.forEach(command => {
+        switch (command.type) {
+            case 'forward':
+                coordinates.x = coordinates.x + command.amount;
+                coordinates.y = coordinates.y + coordinates.aim * command.amount;
+                break;
+            case 'down':
+                coordinates.aim = coordinates.aim + command.amount;
+                break;
+            case 'up':
+                coordinates.aim = coordinates.aim - command.amount;
+                break;
+        }
+    })
+
+    console.log(coordinates);
+    console.log(coordinates.x * coordinates.y);
+}
+
+main_part1();
