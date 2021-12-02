24423d5b463338e469be474b8cce618cdb842f05
emilienbai <bai.emilien+github@gmail.com>
2021-12-02 09:08:43 +0100

    Fix part 2

--- day_1/main_part2.js
+++ day_1/main_part2.js
@@ -5,7 +5,7 @@ function main_part2() {
     const fileAsArray = inputFile.toString().split('\n').map(input => Number(input));
     let increaseCount = 0;
     for (let i = 3; i < fileAsArray.length; i++) {
-        if (fileAsArray[i] + fileAsArray[i - 1] + fileAsArray[i - 2] > fileAsArray[i - 1] + fileAsArray[i - 2] + fileAsArray[i - 3]) {
+        if (fileAsArray[i] > fileAsArray[i - 3]) {
             increaseCount++;
         }
     }
