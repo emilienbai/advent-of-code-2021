b0aa87740c04a56c99e3ce5f2645534e9a8202dd
emilienbai <bai.emilien+github@gmail.com>
2021-12-03 19:15:18 +0100

    Apply linter on day1 + day2

--- day_1/main_part1.js
+++ day_1/main_part1.js
@@ -1,15 +1,18 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function main_part1() {
-    const inputFile = fs.readFileSync('input.txt')
-    const fileAsArray = inputFile.toString().split('\n').map(input => Number(input));
-    let increaseCount = 0;
-    for (let i = 1; i < fileAsArray.length; i++) {
-        if (fileAsArray[i] > fileAsArray[i-1]){
-            increaseCount++;
-        }
+  const inputFile = fs.readFileSync("input.txt");
+  const fileAsArray = inputFile
+    .toString()
+    .split("\n")
+    .map((input) => Number(input));
+  let increaseCount = 0;
+  for (let i = 1; i < fileAsArray.length; i++) {
+    if (fileAsArray[i] > fileAsArray[i - 1]) {
+      increaseCount++;
     }
-    console.log(increaseCount);
+  }
+  console.log(increaseCount);
 }
 
 main_part1();
--- day_1/main_part2.js
+++ day_1/main_part2.js
@@ -1,15 +1,18 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function main_part2() {
-    const inputFile = fs.readFileSync('input.txt')
-    const fileAsArray = inputFile.toString().split('\n').map(input => Number(input));
-    let increaseCount = 0;
-    for (let i = 3; i < fileAsArray.length; i++) {
-        if (fileAsArray[i] > fileAsArray[i - 3]) {
-            increaseCount++;
-        }
+  const inputFile = fs.readFileSync("input.txt");
+  const fileAsArray = inputFile
+    .toString()
+    .split("\n")
+    .map((input) => Number(input));
+  let increaseCount = 0;
+  for (let i = 3; i < fileAsArray.length; i++) {
+    if (fileAsArray[i] > fileAsArray[i - 3]) {
+      increaseCount++;
     }
-    console.log(increaseCount);
+  }
+  console.log(increaseCount);
 }
 
 main_part2();
