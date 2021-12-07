9df202d27182141ddb03177cac2104ba4cbb6471
emilienbai <bai.emilien+github@gmail.com>
2021-12-07 20:00:03 +0100

    Day7: bruteforce is life, bruteforce is love

---
+++ day_7/part1.ts
@@ -0,0 +1,32 @@
+import * as fs from "fs";
+
+function main_part1() {
+  const initialPositions = fs
+    .readFileSync("day_7/input.txt")
+    .toString()
+    .split(",")
+    .map((value: string) => parseInt(value));
+
+  const amountPerPosition: { [initialPosition: number]: number } = {};
+  for (const position of initialPositions) {
+    if (!amountPerPosition[position]) {
+      amountPerPosition[position] = 1;
+    } else {
+      amountPerPosition[position] = amountPerPosition[position] + 1;
+    }
+  }
+
+  const costPerPosition: { [position: number]: number } = {};
+  for (let currentPosition = 0; currentPosition < 2000; currentPosition += 1) {
+    let cost = 0;
+    for (const [position, amount] of Object.entries(amountPerPosition)) {
+      cost += Math.abs(parseInt(position) - currentPosition) * amount;
+    }
+    costPerPosition[currentPosition] = cost;
+  }
+
+  console.log(costPerPosition);
+  console.log(Math.min(...Object.values(costPerPosition)));
+}
+
+main_part1();
